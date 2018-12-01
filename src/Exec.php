e20fb2e610bf285cabfe2ee4162bbfa23805d622
Igor Tarasov <develop@dicr.org>
2018-12-01 16:57:04 +0500

    Initial commit

---
+++ src/Exec.php
@@ -0,0 +1,23 @@
+<?php 
+namespace dicr\exec;
+
+/**
+ * Выполняет команды
+ * 
+ * @author Igor (Dicr) Tarasov <develop@dicr.org>
+ * @version 180626
+ */
+interface Exec {
+	
+	/**
+	 * Выполняет внешнюю команду.
+	 * 
+	 * @param string $cmd команда
+	 * @param array $args аргументы
+	 * @param array $options опции функции
+	 * - escape bool - выполнить экранирование аргументов, false
+	 * @throws \app\lib\exec\ExecException
+	 * @return string вывод команды
+	 */
+	public function run(string $cmd, array $args=[], array $options=[]);
+}
