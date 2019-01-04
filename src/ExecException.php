98d8f75f891701c56971b0e8248c26bd24820b41
Igor Tarasov <develop@dicr.org>
2019-01-05 03:19:39 +0500

    yii2-extension

--- src/ExecException.php
+++ src/ExecException.php
@@ -1,14 +1,14 @@
-<?php 
+<?php
 namespace dicr\exec;
 
 use yii\base\Exception;
 
 /**
  * Ошибка выполнения команды
- * 
+ *
  * @author Igor (Dicr) Tarasov <develop@dicr.org>
  * @version 180626
  */
-class ExecException extends Exception {
-	
+class ExecException extends Exception
+{
 }
